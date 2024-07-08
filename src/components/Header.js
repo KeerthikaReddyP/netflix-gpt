2d0bb6b19a4669f3ed4ef44a8855398534ad742d
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-08 18:44:35 +0530

    Backgound image and Netflix logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="m-4 px-8 py-2 absolute bg-gradient-to-b from-black">
+      <img
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="Netflix logo"
+        className="w-44"
+      />
+    </div>
+  );
 };
 
 export default Header;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./Header";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="">
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/0552717c-9d8c-47bd-9640-4f4efa2de663/537e2c5e-c750-4d4c-9f7a-e66fe93eb977/IN-en-20240701-POP_SIGNUP_TWO_WEEKS-perspective_WEB_b00eeb83-a7e8-4b5b-8ff7-86ed92c51caf_large.jpg"
+          alt="background"
+          className=""
+        />
+      </div>
+      <form></form>
+    </div>
+  );
 };
 
 export default LoginPage;
