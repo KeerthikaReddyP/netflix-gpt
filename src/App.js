55517a6bebe4f8498a9a5853015756bc564d2476
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-08 08:15:02 +0530

    Little css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div className="">
-      Namaste Everyone, Let's build netflixGPT!!
+    <div className="m-4 p-4 text-center">
+      <h1 className="m-2 p-2 text-2xl font-bold text-green-800">Namaste Everyone, Let's build netflixGPT!!</h1>
     </div>
   );
 }
