9e849ce5654905a453c8fcea9e9d7df148a4a097
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-08 18:18:41 +0530

    Header and LoginPage inside Body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import LoginPage from "./LoginPage";
 
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div>
+      <Header />
+      <LoginPage />
+    </div>
+  );
 };
 
 export default Body;
