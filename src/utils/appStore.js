23a6751bb8f16f9c8fca9ff91b85da35d981c665
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-09 19:38:16 +0530

    Adding userSlice to appStore

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useReducer } from "react";
 
 const appStore=configureStore({
-    reducer:{},
+    reducer:{
+        user: useReducer,
+    },
 });
 
 export default appStore;
