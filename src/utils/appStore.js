343dd2fac203c7b8d2fde368ec27ef0bbed8f43c
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-09 19:38:46 +0530

    sry spelling mistake(userSlice)

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { useReducer } from "react";
+import { userReducer } from "react";
 
 const appStore=configureStore({
     reducer:{
-        user: useReducer,
+        user: userReducer,
     },
 });
 
