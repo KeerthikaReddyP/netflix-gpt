664c002c982ed39ad15ffaa38237de4c474b9f9d
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-09 19:37:18 +0530

    created userSlice

---
+++ src/utils/userSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice=createSlice({
+    name:'User',
+    initialState: null,
+    reducers:{
+        addUser: (state,action)=>{
+            return action.payload;
+        },
+        removeUser:()=>{
+            return null;
+        }
+    }
+});
+
+export const {addUser,removeUser}=userSlice.actions;
+
+export default userSlice.reducer;
