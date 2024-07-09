601ff4ebf31fb00d390b7f2d961f6c7e5620388d
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-09 19:48:37 +0530

    huuff

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userReducer } from "./userSlice";
+import userReducer from "./userSlice";
 
 const appStore=configureStore({
     reducer:{
-        user: userReducer,
+        user: userReducer
     },
 });
 
