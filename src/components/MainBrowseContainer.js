594c3f36dc99b699989c035f5c86dd35e62fd961
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 19:19:00 +0530

    getting main movie data

--- src/components/MainBrowseContainer.js
+++ src/components/MainBrowseContainer.js
@@ -1,10 +1,19 @@
 import React from "react";
 import VideoTitle from "./VideoTitle";
 import VideoBackground from "./VideoBackground";
+import { useSelector } from "react-redux";
 
 const MainBrowseContainer = () => {
+
+    const movies=useSelector((store)=>store.movies?.nowPlayingMovies);
+
+    if(!movies) return;
+
+    const mainMovie=movies[0];
+    const {original_title, overview}=mainMovie;
+
   return <div>
-    <VideoTitle />
+    <VideoTitle title={original_title} overview={overview}/>
     <VideoBackground />
   </div>;
 };
