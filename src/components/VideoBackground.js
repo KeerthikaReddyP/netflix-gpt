9a880a8bcf0285301a130ecfda0bb2ca8f590e6e
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 21:39:32 +0530

    finding trailer

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -3,10 +3,14 @@ import { API_OPTIONS } from "../utils/constants";
 
 const VideoBackground = ({movieId}) => {
 
+  const fetchurl="https://api.themoviedb.org/3/movie/"+movieId+"/videos?language=en-US";
+
   const getMovieVideos=async ()=>{
-    const data=await fetch("https://api.themoviedb.org/3/movie/1022789/videos?language=en-US", API_OPTIONS);
+    const data=await fetch(fetchurl, API_OPTIONS);
     const jsonData=await data.json();
-    console.log(jsonData);
+    
+    const filterVideoResult=jsonData.results.filter((video)=>(video.type==="Trailer"));
+    console.log(filterVideoResult);
   };
 
   useEffect(()=>{
