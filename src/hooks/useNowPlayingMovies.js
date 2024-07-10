78a5b7059e5bc4183a373ae90541bd709e76c526
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 18:20:26 +0530

    separate folder for hooks

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
-import { API_OPTIONS } from "./constants";
-import { addNowPlayingMovies } from "./moviesSlice";
+import { API_OPTIONS } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
 
 
