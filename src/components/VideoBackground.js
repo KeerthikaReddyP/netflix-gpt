4d37bc8b4b73614cb2e49285c08a85343f30ba05
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 23:18:45 +0530

    background video autoplay and mute

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -11,7 +11,7 @@ const VideoBackground = ({ movieId }) => {
     <div>
       <iframe
         className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/" + trailerKey}
+        src={"https://www.youtube.com/embed/" + trailerKey + "?&autoplay=1&mute=1"}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
