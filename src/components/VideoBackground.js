db9cae746b929dbbfb80476924a64120780637e9
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 23:14:37 +0530

    styling

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -10,6 +10,7 @@ const VideoBackground = ({ movieId }) => {
   return (
     <div>
       <iframe
+        className="w-screen aspect-video"
         src={"https://www.youtube.com/embed/" + trailerKey}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-const VideoTitle = ({title, overview}) => {
+const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-28 p-10">
+    <div className="w-screen aspect-video pt-[15%] p-10 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-3xl font-bold m-2 p-2">{title}</h1>
       <p className="m-2 p-2 w-1/4">{overview}</p>
       <div className="m-2 p-2">
-        <button className="bg-red-600 text-red-100 py-2 px-6 text-lg font-semibold rounded-md mr-2">Play</button>
-        <button className="bg-red-600 text-red-100 py-2 px-6 text-lg font-semibold rounded-md ml-2">More info</button>
+        <button className="bg-white text-black py-2 px-6 text-lg font-bold rounded-md mr-2 hover:bg-opacity-80">
+          ▷Play
+        </button>
+        <button className="bg-gray-600 text-white py-2 px-6 text-lg font-semibold rounded-md ml-2 hover:bg-opacity-80">
+          More info
+        </button>
       </div>
     </div>
   );
