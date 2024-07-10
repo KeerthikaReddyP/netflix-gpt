1c351e1505f85872b7e331b8727513b202f6fe0e
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 21:16:01 +0530

    get movie videos API

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
 
-const VideoBackground = () => {
-  return <div>Video Background</div>;
+const VideoBackground = ({movieId}) => {
+
+  const getMovieVideos=async ()=>{
+    const data=await fetch("https://api.themoviedb.org/3/movie/1022789/videos?language=en-US", API_OPTIONS);
+    const jsonData=await data.json();
+    console.log(jsonData);
+  };
+
+  useEffect(()=>{
+    getMovieVideos();
+  },[]);
+
+  return <div></div>;
 };
 
 export default VideoBackground;
