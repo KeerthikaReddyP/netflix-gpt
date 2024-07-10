8f437e1bccbc2f80375b168328fdcc1e5fdb9fbe
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 19:20:16 +0530

    video title

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const VideoTitle = () => {
-  return <div>VideoTitle</div>;
+const VideoTitle = ({title, overview}) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{overview}</p>
+    </div>
+  );
 };
 
 export default VideoTitle;
