f8791d36ca7de178312f14a4589ebb75c5292bc6
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 22:07:50 +0530

    dispatched trailer video

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,20 +1,21 @@
 import React, { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
 
 const VideoBackground = ({ movieId }) => {
-  const fetchurl =
-    "https://api.themoviedb.org/3/movie/" + movieId + "/videos?language=en-US";
+  const dispatch = useDispatch();
+
+  const fetchurl = "https://api.themoviedb.org/3/movie/" + movieId + "/videos?language=en-US";
 
   const getMovieVideos = async () => {
     const data = await fetch(fetchurl, API_OPTIONS);
     const jsonData = await data.json();
 
-    const filterVideoResult = jsonData.results.filter(
-      (video) => video.type === "Trailer"
-    );
-    const trailerVideo = filterVideoResult.length
-      ? filterVideoResult[0]
-      : jsonData.results[0];
+    const filterVideoResult = jsonData.results.filter((video) => video.type === "Trailer");
+    const trailerVideo = filterVideoResult.length? filterVideoResult[0]: jsonData.results[0];
+
+    dispatch(addTrailerVideo(trailerVideo));
   };
 
   useEffect(() => {
@@ -24,7 +25,7 @@ const VideoBackground = ({ movieId }) => {
   return (
     <div>
       <iframe
-        src={"https://www.youtube.com/embed/"+trailerVideo.key}
+        src={"https://www.youtube.com/embed/" + trailerVideo.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
