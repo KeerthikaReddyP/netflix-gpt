83cfa0f0d4be2c4a831d08e78f345d2474e87248
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 18:56:26 +0530

    created main and secondary browse container components

---
+++ src/components/MainBrowseContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainBrowseContainer = () => {
+  return <div>Main Browse COntainer</div>;
+};
+
+export default MainBrowseContainer;
---
+++ src/components/SecondaryBrowseContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SecondaryBrowseContainer = () => {
+  return <div>Secondary Browse COntainer</div>;
+};
+
+export default SecondaryBrowseContainer;
