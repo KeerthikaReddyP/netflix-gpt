aa324930210ed1cbdb2c2f0a85a1f5223b43df9a
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 16:34:00 +0530

    Added api options

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,12 @@ export const USER_PHOTO =
 
 export const NETFLIX_BACKGROUND_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/0552717c-9d8c-47bd-9640-4f4efa2de663/537e2c5e-c750-4d4c-9f7a-e66fe93eb977/IN-en-20240701-POP_SIGNUP_TWO_WEEKS-perspective_WEB_b00eeb83-a7e8-4b5b-8ff7-86ed92c51caf_large.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzM2OWZmZmY2NDAxMWE5MDQ3OGVmZWZlODg1MTcwZSIsIm5iZiI6MTcyMDYwNzM0MS42NDEzNSwic3ViIjoiNjY4ZTVlNDc5ZDA3N2Y4N2Y2NTlkYzk1Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.OyVVpMagJ7JxuPpbt2cqea4dWnixon3ywEsUabZYSaE",
+  },
+};
