884b647beef0b37cf9564f97b91b3c2167e47f64
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 16:39:25 +0530

    TMDB now playing movies API call

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import { API_OPTIONS } from "../utils/constants";
 
 const Browse = () => {
+
+  const getNowPlayingMovies=async ()=>{
+    const data=await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
+    const jsonData=await data.json();
+    console.log(jsonData);
+  }
+
+  useEffect(()=>{
+    getNowPlayingMovies();
+  },[]);
+
   return (
     <div>
       <Header />
