957cdbde4da2d09d8558823f83d5a86cf23616b3
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-10 22:04:46 +0530

    Putting trailer in our store

--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,10 +4,14 @@ const moviesSlice=createSlice({
     name:'movies',
     initialState:{
         nowPlayingMovies:null,
+        trailerVideo:null,
     },
     reducers:{
         addNowPlayingMovies:(state,action)=>{
             state.nowPlayingMovies=action.payload;
+        },
+        addTrailerVideo:(state,action)=>{
+            state.trailerVideo=action.payload;
         },
     }
 });
