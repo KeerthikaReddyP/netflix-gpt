df7c920445d8546162f6ec45064addc549c61ce1
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-11 11:25:26 +0530

    usePopularMovies (Custom Hook)

--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,6 +4,7 @@ const moviesSlice=createSlice({
     name:'movies',
     initialState:{
         nowPlayingMovies:null,
+        popularMovies:null,
         trailerVideo:null,
     },
     reducers:{
@@ -13,9 +14,12 @@ const moviesSlice=createSlice({
         addTrailerVideo:(state,action)=>{
             state.trailerVideo=action.payload;
         },
+        addPopularMovies:(state,action)=>{
+            state.addPopularMovies=action.payload;
+        },
     }
 });
 
 
-export const {addNowPlayingMovies, addTrailerVideo}=moviesSlice.actions;
+export const {addNowPlayingMovies, addTrailerVideo, addPopularMovies}=moviesSlice.actions;
 export default moviesSlice.reducer;
