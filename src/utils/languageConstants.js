261f165a058ab8c7e093b76050fb21d251ad38ee
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-12 20:17:52 +0530

    language constants file

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,21 @@
+
+const lang={
+    en:{
+        search: "Search",
+        gptSearchPlaceholder: "What's on your mind?",
+    },
+    hindi:{
+        search: "खोज",
+        gptSearchPlaceholder: "आपके दिमाग में क्या है?",
+    },
+    tel:{
+        search: "వెతకండి",
+        gptSearchPlaceholder: "మీ మనసులో ఏముంది?",
+    },
+    spanish:{
+        search: "Buscar",
+        gptSearchPlaceholder: "¿Qué tienes en mente?",
+    },
+};
+
+export default lang;
