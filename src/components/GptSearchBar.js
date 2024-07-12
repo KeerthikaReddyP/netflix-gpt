7429f2f8525d4cadd682b9c211e4cc37ef798175
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-12 17:42:59 +0530

    styling gpt search bar

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -2,10 +2,16 @@ import React from "react";
 
 const GptSearchBar = () => {
   return (
-    <div>
-      <form>
-        <input type="text" placeholder="What's on your mind?" />
-        <button>Search</button>
+    <div className="pt-[10%] flex justify-center">
+      <form className="bg-black w-1/2 grid grid-cols-12">
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          className="border border-black m-4 py-2 px-4 col-span-9"
+        />
+        <button className="bg-red-600 text-white font-bold m-4 py-2 px-4 rounded-md col-span-3">
+          Search
+        </button>
       </form>
     </div>
   );
