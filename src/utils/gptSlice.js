357934e4a05d7804ab141f6c3d357999b0f203a7
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-12 17:12:30 +0530

    created gptSlice

---
+++ src/utils/gptSlice.js
@@ -0,0 +1,16 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const gptSlice=createSlice({
+    name:"gpt",
+    initialState:{
+        showGptPage:false
+    },
+    reducers:{
+        toggleGptPageView:(state,action)=>{
+            state.showGptPage=!(state.showGptPage);
+        }
+    }
+});
+
+export const {toggleGptPageView}=gptSlice.actions;
+export default gptSlice.reducer;
