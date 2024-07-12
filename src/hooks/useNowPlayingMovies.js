972ddf0c481ba6c546d995297545ba0746fe2e7a
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-12 17:05:26 +0530

    Try catch while fetching

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -3,17 +3,22 @@ import { API_OPTIONS } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
 
-
 const useNowPlayingMovies = () => {
   const dispatch = useDispatch();
 
   const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const jsonData = await data.json();
-    dispatch(addNowPlayingMovies(jsonData.results));
+    try {
+      console.log("Started fetching");
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?page=1",
+        API_OPTIONS
+      );
+      console.log(data);
+      const jsonData = await data.json();
+      dispatch(addNowPlayingMovies(jsonData.results));
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
