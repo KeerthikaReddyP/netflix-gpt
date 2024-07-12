e72aed581389f8a27fe231a4febbdcf943c31e93
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-12 20:19:42 +0530

    testing multi language

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import lang from "../utils/languageConstants";
 
 const GptSearchBar = () => {
   return (
@@ -6,11 +7,11 @@ const GptSearchBar = () => {
       <form className="bg-black w-1/2 grid grid-cols-12">
         <input
           type="text"
-          placeholder="What's on your mind?"
+          placeholder={lang.hindi.gptSearchPlaceholder}
           className="border border-black m-4 py-2 px-4 col-span-9"
         />
         <button className="bg-red-600 text-white font-bold m-4 py-2 px-4 rounded-md col-span-3">
-          Search
+          {lang.tel.search}
         </button>
       </form>
     </div>
