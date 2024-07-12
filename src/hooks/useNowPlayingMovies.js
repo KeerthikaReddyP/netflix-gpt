a3563f703bbc04c01ff42a9dcf68d20037264066
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-12 17:06:23 +0530

    removed console logs

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -8,12 +8,10 @@ const useNowPlayingMovies = () => {
 
   const getNowPlayingMovies = async () => {
     try {
-      console.log("Started fetching");
       const data = await fetch(
         "https://api.themoviedb.org/3/movie/now_playing?page=1",
         API_OPTIONS
       );
-      console.log(data);
       const jsonData = await data.json();
       dispatch(addNowPlayingMovies(jsonData.results));
     } catch (err) {
