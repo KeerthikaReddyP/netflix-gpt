e74d2f6f4bb524b520c8d0386067f02a26f97dd6
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-07-12 18:26:48 +0530

    background image added to gpt page

--- src/components/GptPage.js
+++ src/components/GptPage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import GptSearchBar from "./GptSearchBar";
 import GptMovieSuggestions from "./GptMovieSuggestions";
+import {NETFLIX_BACKGROUND_IMAGE} from "../utils/constants";
 
 const GptPage = () => {
   return (
     <div>
+      <div>
+        <img alt="netflix background" src={NETFLIX_BACKGROUND_IMAGE} />
+      </div>
         <GptSearchBar />
         <GptMovieSuggestions />
     </div>
